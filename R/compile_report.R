55867a0107d179efb19cda701f582604f6a33e1b
Thibaut Jombart <thibautjombart@gmail.com>
2018-06-14 12:29:58 +0100

    adding quiet argument and doc

--- R/compile_report.R
+++ R/compile_report.R
@@ -1,13 +1,25 @@
-
-## This function will compile a report whose path or short name is provided.
-
+#' Compile an rmarkdown report
+#'
+#' This function can be used to compile a report using its name, or a partial
+#' match for its name. The full path needs not be specified, but reports are
+#' expected to be inside the \code{report_sources} folder (or any subfolder
+#' within). Outputs will be generated in a named and time-stamped directory
+#' within \code{report_outputs}.
+#'
+#' @export
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#' 
 #' @param file the full path, or a partial, unambiguous match for the Rmd
-#'   report to be compiled
+#'   report to be compiled.
 #'
-#' @param ... further arguments passed to \code{rmarkdown::render}
+#' @param quiet a logical indicating if messages from rmarkdown compilation
+#'   should be displayed; \code{FALSE} by default.
+#'
+#' @param ... further arguments passed to \code{rmarkdown::render}.
 #'
 
-compile_report <- function(file, ...) {
+compile_report <- function(file, quiet = TRUE, ...) {
   if (!require("here")) {
     stop("package 'here' is not installed")
   }
@@ -44,14 +56,19 @@ compile_report <- function(file, ...) {
   files_before <- unique(sub("~$", "", files_before))
 
   cat(sprintf("\n/// compiling report: '%s'", shorthand))
-  rmarkdown::render(rmd_path, ...)
+  output_file <- rmarkdown::render(rmd_path, quiet = quiet, ...)
   cat(sprintf("\n/// '%s' done!\n", shorthand))
 
   files_after <- dir(here::here("report_sources"))
   files_after <- unique(sub("~$", "", files_after))
   new_files <- setdiff(files_after,
                        files_before)
+  new_files <- unique(c(new_files, output_file))
 
+  if (!dir.exists(here::here("report_ouputs"))) {
+    dir.create(here::here("report_ouputs"))
+  }
+  
   datetime <- sub(" ", "_", as.character(Sys.time()))
   report_dir <- paste0(here("report_outputs"),
                        "/", base_name, "_", date)
