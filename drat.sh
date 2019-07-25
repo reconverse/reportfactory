187753d58888d81ba962785e4c3be55f841f42af
Steph Locke <stephanie.g.locke@gmail.com>
2019-07-25 17:49:54 +0100

    Create drat.sh

---
+++ drat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit -o nounset
+addToDrat(){
+  mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GITHUB_PAT@github.com/samplerecon/recondrat.git"
+  git fetch upstream
+  git checkout master
+
+  Rscript -e "drat::insertPackage('../$PKG_TARBALL', repodir = './drat')"
+  git add --all
+  git commit -m "add $PKG_TARBALL (build $TRAVIS_BUILD_ID)"
+  git push
+
+}
+addToDrat
