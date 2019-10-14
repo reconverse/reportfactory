78845c5c0d0b882f71db1c731ec016a0fa69688d
Amy Gimma <amyg225@gmail.com>
2019-10-14 09:59:48 +0200

    Clean up base_bind_rows with annotations

--- R/base_bind_rows.R
+++ R/base_bind_rows.R
@@ -2,10 +2,12 @@
   # and then base::rbind the new df (the function doesn't work with diff cols)
 
 # Steps:
-  # 1. Add missing cols to dfs by setdiff ouput & nrows of other df
-  # 2. Gsub `.` to `_` in df names 
-  # 3. Use base::rbind to combine dfs
+  # 1. Get set diff of col names, change `.` to `_` in df col names 
+  # 2. Add missing cols to each data frame by using a setdiff on col names
+  # 3. Use base::rbind to combine dfs, which now have same cols (filled with NA)
+
 base_bind_rows <- function(df1, df2) {
+  # S1. Get set diff of col names, change `.` to `_` in df col names
   names_df1 <- lapply(names(df1), gsub, pattern = "\\.", replacement = "_")
   names_df1 <- unlist(names_df1)
   names(df1) <- names_df1
@@ -18,6 +20,7 @@ base_bind_rows <- function(df1, df2) {
   set_diff1 <- setdiff(names_df1, names_df2)
   set_diff2 <- setdiff(names_df2, names_df1)
   
+  # S2. Add missing cols to each data frame by using a setdiff on col names
   empty_df1 <- as.data.frame(matrix(ncol = length(set_diff2), nrow = nrow(df2)))
   names(empty_df1) <- set_diff2
   mdf1 <- merge(df1, empty_df1)
@@ -26,9 +29,8 @@ base_bind_rows <- function(df1, df2) {
   names(empty_df2) <- set_diff1
   mdf2 <- merge(df2, empty_df2)
     
-
+  # 3. Use base::rbind to combine dfs, which now have same cols (filled with NA)
   all_rows <- rbind(mdf1, mdf2)
-  # all_rows <- unlist(all_rows)
   names_all <- lapply(names(all_rows), gsub, pattern = "\\.", replacement = "_")
   names(all_rows) <-  unlist(names_all)
    
