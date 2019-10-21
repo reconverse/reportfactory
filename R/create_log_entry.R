52b432820b32872af603a590f7fceab026ba9cab
Amy Gimma <amyg225@gmail.com>
2019-10-21 15:13:42 +0200

    Add logging annotation and minor code cleans

--- R/create_log_entry.R
+++ R/create_log_entry.R
@@ -1,3 +1,17 @@
+## This function takes relevant info from an environment object created in the  
+##   `compile_reports` function, which is used as a cleaned `.compile_log` entry.
+##    This function makes it clear which variables will be logged and contains
+##    the logging logic in one place.
+## Notes:
+##   - `compile_init_env` should be set at the beginning of the `compile_function`
+##      to record the arguments and environment when the `compile_reprots` function 
+##      is called
+##   - `dots` and `timestamp` variables should be set at the beginning of the 
+##      compile function, so that they are only called once and used as-needed
+##      throughout the function
+##   - `report_source_file` and `output_files` are stored as obects in the 
+##     `compile_report` environment, and extracted from the env object here
+
 create_log_entry <- function(env_list) {
   log_entry <- list()
   log_entry$compile_init_env <- env_list$compile_init_env
--- R/current_compile_log.R
+++ R/current_compile_log.R
@@ -1,7 +1,7 @@
-current_compile_log <- function(log_file_path = getwd(), base_name) {
-  no_log_file <- sum(file.exists(log_file_path, hidden.files = TRUE)) == 0
+current_compile_log <- function(log_file_path = getwd(), base_name, date_string) {
+  no_log_file <- !file.exists(log_file_path, hidden.files = TRUE)
   if (no_log_file) {
-    initialize_log <- list(initialize = TRUE, timestamp = Sys.time())
+    initialize_log <- list(initialize = TRUE, timestamp = date_string)
     attr(initialize_log, "factory_name") <- base_name
     saveRDS(initialize_log, log_file_path)
   }
