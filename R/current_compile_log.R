473354439833563c0a724d075feb2b759a7705fa
Amy Gimma <amyg225@gmail.com>
2019-10-24 18:02:36 +0100

    address warning from current_compile_log

--- R/current_compile_log.R
+++ R/current_compile_log.R
@@ -1,6 +1,6 @@
 current_compile_log <- function(log_file_path = getwd(), base_name, date_string) {
-  no_log_file <- !file.exists(log_file_path, hidden.files = TRUE)
-  if (no_log_file) {
+  no_log_file <- file.exists(log_file_path, hidden.files = TRUE)
+  if (sum(no_log_file) == 0) {
     initialize_log <- list(initialize = TRUE, timestamp = date_string)
     attr(initialize_log, "factory_name") <- base_name
     saveRDS(initialize_log, log_file_path)
