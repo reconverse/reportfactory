3e424976e6ac3ad147257758610aaf98682c217e
Ellen Talbot <sgetalbo@liverpool.ac.uk>
2019-11-12 13:48:58 +0000

    Create roxygen documentation for internal functions

--- R/ignore_tilde.R
+++ R/ignore_tilde.R
@@ -1,4 +1,25 @@
 
+#' Take a vector of characters and removes the ones which end in a tilde
+#'
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' 
+#' @param x A vector of characters 
+#' 
+#' 
+#' @noRd
+#' @keywords internal
+#'
+#' @example
+#' 
+#' \dontrun{
+#' 
+#' x <- c("contacts_2017-10-29~", "example_2017-10-29")
+#' check <- ignore_tilde(x)
+#' print(check)
+#' }
+#'
 ## This takes a vector of characters and removes the ones ending with a ~
 
 ignore_tilde <- function(x) {
