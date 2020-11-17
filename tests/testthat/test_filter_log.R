9764c47e062ab3252fbe85d1db574f3c065f79cf
Thibaut Jombart <thibautjombart@gmail.com>
2020-11-17 04:15:23 +0000

    commenting out examples for log/shipping

--- tests/testthat/test_filter_log.R
+++ tests/testthat/test_filter_log.R
@@ -1,148 +1,148 @@
-context("Test log filtering")
-odir <- getwd()
-on.exit(setwd(odir))
+## context("Test log filtering")
+## odir <- getwd()
+## on.exit(setwd(odir))
 
-setwd(tempdir())
-random_factory(include_examples = TRUE)
-source_name <- "foo"
-report_source_file_name <- list_reports(pattern = source_name)[1]
-compile_report(
-  report_source_file_name,
-  quiet = TRUE,
-  params = list(other = "test"))
+## setwd(tempdir())
+## random_factory()
+## source_name <- "foo"
+## report_source_file_name <- list_reports(pattern = source_name)[1]
+## compile_report(
+##   report_source_file_name,
+##   quiet = TRUE,
+##   params = list(other = "test"))
 
-compile_report(
-  report_source_file_name, 
-  quiet = FALSE, 
-  params = list("other" = "two",
-                "more" = list("thing" = "foo"))
-)
+## compile_report(
+##   report_source_file_name, 
+##   quiet = FALSE, 
+##   params = list("other" = "two",
+##                 "more" = list("thing" = "foo"))
+## )
 
-log_file <- readRDS(".compile_log.rds")
-
-
+## log_file <- readRDS(".compile_log.rds")
 
 
 
-test_that("Filtering returns only exact matches on EXACT parameters", {
-  skip_on_cran()
+
+
+## test_that("Filtering returns only exact matches on EXACT parameters", {
+##   skip_on_cran()
   
-  ## entries missing some params arguments so should return an empty list
-  missing_params_filtered <- filter_log(
-    match_exact = c("params"),
-    log_file = log_file,
-    params = list("other" = "two")
-  )
+##   ## entries missing some params arguments so should return an empty list
+##   missing_params_filtered <- filter_log(
+##     match_exact = c("params"),
+##     log_file = log_file,
+##     params = list("other" = "two")
+##   )
   
-  expect_equal(length(missing_params_filtered), 0)
+##   expect_equal(length(missing_params_filtered), 0)
   
-  ## params args match and should return exactly one log entry
-  non_missing_filtered <- filter_log(
-    match_exact = c("params", "dots"),
-    log_file = log_file,
-    params = list("other" = "two",
-                  "more" = list("thing" = "foo"))
-  )
+##   ## params args match and should return exactly one log entry
+##   non_missing_filtered <- filter_log(
+##     match_exact = c("params", "dots"),
+##     log_file = log_file,
+##     params = list("other" = "two",
+##                   "more" = list("thing" = "foo"))
+##   )
   
-  expect_equal(non_missing_filtered, log_file[-c(1)])
+##   expect_equal(non_missing_filtered, log_file[-c(1)])
   
-})
+## })
 
 
 
 
 
-test_that("Filtering a list with string values returns log entries", {
-  skip_on_cran()
+## test_that("Filtering a list with string values returns log entries", {
+##   skip_on_cran()
   
-  filtered <- filter_log(log_file, 
-                         file = report_source_file_name,
-                         most_recent = FALSE)
+##   filtered <- filter_log(log_file, 
+##                          file = report_source_file_name,
+##                          most_recent = FALSE)
   
-  ## Expect the filtered list to be the same as the log entries of log_file
-  ## (both entries match the filter)
-  expect_equal(names(filtered), names(log_file))
+##   ## Expect the filtered list to be the same as the log entries of log_file
+##   ## (both entries match the filter)
+##   expect_equal(names(filtered), names(log_file))
   
-  filtered <- filter_log(log_file, params = list("other" = "two"))
+##   filtered <- filter_log(log_file, params = list("other" = "two"))
   
-  expect_equal(filtered, log_file[-c(1)])
+##   expect_equal(filtered, log_file[-c(1)])
 
-})
-
-
+## })
 
 
 
-test_that("Filtering with non-string values returns the matching lists", {
-  skip_on_cran()
+
+
+## test_that("Filtering with non-string values returns the matching lists", {
+##   skip_on_cran()
   
-  filtered <- filter_log(
-    log_file = log_file,
-    params = list("other" = "two"),
-    most_recent = FALSE)
+##   filtered <- filter_log(
+##     log_file = log_file,
+##     params = list("other" = "two"),
+##     most_recent = FALSE)
 
-  expect_equal(filtered, log_file[-c(1)])
+##   expect_equal(filtered, log_file[-c(1)])
 
-})
-
-
+## })
 
 
 
-test_that("Filtering for most recent returns only last match for each source", {
-  source_name <- "contacts"
-  report_source_file_name <- list_reports(pattern = source_name)[1]
-  compile_report(
-    report_source_file_name,
-    quiet = TRUE,
-    params = list(other = "test"))
-  log_file <- readRDS(".compile_log.rds")
+
+
+## test_that("Filtering for most recent returns only last match for each source", {
+##   source_name <- "contacts"
+##   report_source_file_name <- list_reports(pattern = source_name)[1]
+##   compile_report(
+##     report_source_file_name,
+##     quiet = TRUE,
+##     params = list(other = "test"))
+##   log_file <- readRDS(".compile_log.rds")
   
-  most_recent_filtered <- filter_log(log_file, most_recent = TRUE)
+##   most_recent_filtered <- filter_log(log_file, most_recent = TRUE)
   
-  ## Without `most_recent = TRUE` this would be equal to log_file[c(1,2,3)]
-  expect_equal(most_recent_filtered, log_file[c(2,3)])
+##   ## Without `most_recent = TRUE` this would be equal to log_file[c(1,2,3)]
+##   expect_equal(most_recent_filtered, log_file[c(2,3)])
   
-})
+## })
 
 
 
 
 
-test_that("Filtering can return outputs only", {
-  outputs_only_filtered <- filter_log(log_file, 
-                                      file = report_source_file_name,
-                                      outputs_only = TRUE,
-                                      most_recent = FALSE)
+## test_that("Filtering can return outputs only", {
+##   outputs_only_filtered <- filter_log(log_file, 
+##                                       file = report_source_file_name,
+##                                       outputs_only = TRUE,
+##                                       most_recent = FALSE)
   
-  expect_equal(names(outputs_only_filtered[[1]]), "output_files")
+##   expect_equal(names(outputs_only_filtered[[1]]), "output_files")
 
-})
-
-
+## })
 
 
 
-test_that("Filtering can return specific outputs only", {
-  source_name <- "foo"
-  report_source_file_name <- list_reports(pattern = source_name)[1]
-  output_types <- c("png", "csv")
-  outputs_only_filtered <- filter_log(log_file, 
-                                      file = report_source_file_name,
-                                      outputs_only = TRUE,
-                                      output_file_types = output_types,
-                                      most_recent = TRUE)
+
+
+## test_that("Filtering can return specific outputs only", {
+##   source_name <- "foo"
+##   report_source_file_name <- list_reports(pattern = source_name)[1]
+##   output_types <- c("png", "csv")
+##   outputs_only_filtered <- filter_log(log_file, 
+##                                       file = report_source_file_name,
+##                                       outputs_only = TRUE,
+##                                       output_file_types = output_types,
+##                                       most_recent = TRUE)
   
   
-  remove <- c(".html", ".jpeg", ".pdf", ".rds", ".rmd", ".xls", ".xlsx")
+##   remove <- c(".html", ".jpeg", ".pdf", ".rds", ".rmd", ".xls", ".xlsx")
   
-  outputs_only_entry <- outputs_only_filtered[[1]]
-  outputs_only_entry <- unlist(outputs_only_entry)
-  removed <- lapply(remove, function(r) grep(r, outputs_only_entry))
-  results <- outputs_only_entry[unlist(removed)]
+##   outputs_only_entry <- outputs_only_filtered[[1]]
+##   outputs_only_entry <- unlist(outputs_only_entry)
+##   removed <- lapply(remove, function(r) grep(r, outputs_only_entry))
+##   results <- outputs_only_entry[unlist(removed)]
   
-  expect_equal(length(results), 0)
+##   expect_equal(length(results), 0)
 
-})
+## })
 
-setwd(odir)
+## setwd(odir)
--- tests/testthat/test_ship_reports.R
+++ tests/testthat/test_ship_reports.R
@@ -1,80 +1,80 @@
-context("Test ship reports")
+## context("Test ship reports")
 
-odir <- getwd()
-on.exit(setwd(odir))
+## odir <- getwd()
+## on.exit(setwd(odir))
 
-setwd(tempdir())
-random_factory(include_examples = TRUE)
-source_name <- "foo"
-report_source_file_name <- list_reports(pattern = source_name)[1]
+## setwd(tempdir())
+## random_factory()
+## source_name <- "foo"
+## report_source_file_name <- list_reports(pattern = source_name)[1]
 
-compile_report(
-  report_source_file_name, 
-  quiet = FALSE, 
-  params = list("other" = "two",
-                "more" = list("thing" = "foo"))
-)
+## compile_report(
+##   report_source_file_name, 
+##   quiet = FALSE, 
+##   params = list("other" = "two",
+##                 "more" = list("thing" = "foo"))
+## )
 
-compile_report(
-  report_source_file_name,
-  quiet = TRUE,
-  params = list(other = "test"))
+## compile_report(
+##   report_source_file_name,
+##   quiet = TRUE,
+##   params = list(other = "test"))
 
-compile_report(
-  report_source_file_name, 
-  quiet = FALSE, 
-  params = list("other" = "two",
-                "more" = list("thing" = "foo"))
-)
+## compile_report(
+##   report_source_file_name, 
+##   quiet = FALSE, 
+##   params = list("other" = "two",
+##                 "more" = list("thing" = "foo"))
+## )
 
-log_file <- readRDS(".compile_log.rds")
-length(log_file)
-
-
+## log_file <- readRDS(".compile_log.rds")
+## length(log_file)
 
 
-test_that("Report outputs are copied into a folder at factory root", {
-  skip_on_cran()
+
+
+## test_that("Report outputs are copied into a folder at factory root", {
+##   skip_on_cran()
   
-  ## entries missing some params arguments so should return an empty list
-  ship_reports(
-    params = list("other" = "two"), 
-    most_recent = TRUE
-  )
+##   ## entries missing some params arguments so should return an empty list
+##   ship_reports(
+##     params = list("other" = "two"), 
+##     most_recent = TRUE
+##   )
   
-  factory_pattern <- ".*report_outputs/(.*?)/.*"
-  factory_repl <- "\\1"
-  compile_pattern <- ".*\\/"
-  compile_repl <- ""
+##   factory_pattern <- ".*report_outputs/(.*?)/.*"
+##   factory_repl <- "\\1"
+##   compile_pattern <- ".*\\/"
+##   compile_repl <- ""
   
-  entry_output_dir <- log_file[[length(log_file)]]$output_dir
-  shipped_dir <- grep("shipped_", list.files(), value = TRUE)[1]
-  fact_dir <- gsub(factory_pattern, factory_repl, entry_output_dir)
-  comp_dir <- gsub(compile_pattern, compile_repl, entry_output_dir)
+##   entry_output_dir <- log_file[[length(log_file)]]$output_dir
+##   shipped_dir <- grep("shipped_", list.files(), value = TRUE)[1]
+##   fact_dir <- gsub(factory_pattern, factory_repl, entry_output_dir)
+##   comp_dir <- gsub(compile_pattern, compile_repl, entry_output_dir)
   
-  destination_dir <- file.path(shipped_dir, fact_dir, comp_dir)
-  destination_dir
-  expect_true(dir.exists(destination_dir))
+##   destination_dir <- file.path(shipped_dir, fact_dir, comp_dir)
+##   destination_dir
+##   expect_true(dir.exists(destination_dir))
   
-  shipped_files <- list.files(destination_dir, recursive = TRUE)
+##   shipped_files <- list.files(destination_dir, recursive = TRUE)
   
-  expected_files <- c("boxplots-1.pdf", "boxplots-1.png", "foo_2018-06-29.html", 
-                      "outputs_base.csv", "violins-1.pdf", "violins-1.png")
+##   expected_files <- c("boxplots-1.pdf", "boxplots-1.png", "foo_2018-06-29.html", 
+##                       "outputs_base.csv", "violins-1.pdf", "violins-1.png")
 
-  expect_equal(sort(expected_files), sort(shipped_files))
+##   expect_equal(sort(expected_files), sort(shipped_files))
 
-})
-
-
+## })
 
 
-test_that("ships matches of source file name", {
-  ship_reports(file = report_source_file_name)
+
+
+## test_that("ships matches of source file name", {
+##   ship_reports(file = report_source_file_name)
   
-  shipped_dirs <- grep("shipped_", list.files(), value = TRUE)
-  newest_dir <- shipped_dirs[length(shipped_dirs)]
-  source_name_dir <- grep(source_name, list.files(newest_dir), value = TRUE)
+##   shipped_dirs <- grep("shipped_", list.files(), value = TRUE)
+##   newest_dir <- shipped_dirs[length(shipped_dirs)]
+##   source_name_dir <- grep(source_name, list.files(newest_dir), value = TRUE)
   
-  expect_true(dir.exists(file.path(newest_dir, source_name_dir)))
+##   expect_true(dir.exists(file.path(newest_dir, source_name_dir)))
   
-})
+## })
